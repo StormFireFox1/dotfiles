5e43732ee9ebd89fc4778a2049eb4cfc6a32537f
Matei-Alexandru Gardus <matei@gard.us>
2025-12-07 23:02:52 -0800

    Reseal SMB key
    The SMB key was not sealed with the Machine's host key. This should do
    it.
    
    We're also renaming the Bullshit Machine's user because this will be
    temporary; I'll sync up my SSH keys soon enough to be the same across
    machines, ideally (i.e. hardware key).

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,11 +1,14 @@
 let
-  bullshitMachine = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINPgMul7XW0LoM3ADHGLu/Tkn+XQGKRD+gtcHxiW3IAn";
+  tempBullshitMachineUser = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINPgMul7XW0LoM3ADHGLu/Tkn+XQGKRD+gtcHxiW3IAn";
   stormPrism = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGOW5gIaGw2Y8pzOM9C3G9frgqMf2SfleeUAPeIP0/6S";
+
+  bullshitMachine = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEM4ocxU0ch5PSxPEMRmvUSfFZtclgburTjrEeCEYCFI";
+
   users = [
-    bullshitMachine
+    tempBullshitMachineUser
     stormPrism
   ];
 in
 {
-  "StormDriveSmbKey.age".publicKeys = users;
+  "StormDriveSmbKey.age".publicKeys = users ++ [ bullshitMachine ];
 }
