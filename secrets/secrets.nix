18e402a620719707ccce941498a8a43928b0a708
Matei-Alexandru Gardus <matei@gard.us>
2025-12-30 02:40:10 -0800

    Rekey secrets for StormPrism as well

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,15 +1,19 @@
 let
   tempBullshitMachineUser = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINPgMul7XW0LoM3ADHGLu/Tkn+XQGKRD+gtcHxiW3IAn";
-  stormPrism = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGOW5gIaGw2Y8pzOM9C3G9frgqMf2SfleeUAPeIP0/6S";
+  stormPrismSecretiveKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGOW5gIaGw2Y8pzOM9C3G9frgqMf2SfleeUAPeIP0/6S";
 
   bullshitMachine = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEM4ocxU0ch5PSxPEMRmvUSfFZtclgburTjrEeCEYCFI";
+  stormPrism = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIk+BK0oDAYn3KI8Q62kU1IgPXTUBsRHKUUz/TC4zBxz";
 
   users = [
     tempBullshitMachineUser
-    stormPrism
+    stormPrismSecretiveKey
   ];
 in
 {
   "StormDriveSmbKey.age".publicKeys = users ++ [ bullshitMachine ];
-  "BorgBackupKey.age".publicKeys = users ++ [ bullshitMachine ];
+  "BorgBackupKey.age".publicKeys = users ++ [
+    bullshitMachine
+    stormPrism
+  ];
 }
